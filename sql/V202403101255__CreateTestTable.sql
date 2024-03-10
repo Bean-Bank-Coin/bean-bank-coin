a2b2a6855134b3196d113fe929d8993f57544e1b
Bryce <bryce.grahn@bbd.co.za>
2024-03-10 23:21:46 +0200

    update

--- sql/V202403101255__CreateTestTable.sql
+++
@@ -1,8 +0,0 @@
-USE bean_bank_coin_db;
-
-CREATE TABLE test_table (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50),
-    age INT,
-    email VARCHAR(100)
-);
--- sql/V202404101941__CreateTableFromActions.sql
+++
@@ -1,8 +0,0 @@
-USE public2;
-
-CREATE TABLE hh_table (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50),
-    age INT,
-    email VARCHAR(100)
-);
