19744a200f3b6f31d5c2f7cf0970a7c023aef3da
Bryce <bryce.grahn@bbd.co.za>
2024-03-11 23:48:19 +0200

    fix flyway migration

---
+++ sql/V202403112344__InsertDataIntoBeanTypeTable.sql
@@ -0,0 +1,12 @@
+Use bean_bank_coin_schema;
+
+INSERT INTO `BeanType` (`BeanName`, `BeanSymbol`, `ValueInRands`)
+VALUES
+    ('Green Bean', 'GNB', 5),
+    ('Blue Bean', 'BLB', 10),
+    ('Purple Bean', 'PLB', 15),
+    ('Orange Bean', 'ORB', 20),
+    ('Red Bean', 'RDB', 25),
+    ('Yellow Bean', 'YLB', 30),
+    ('Silver Bean', 'SLB', 35),
+    ('Gold Bean', 'GLB', 40);
