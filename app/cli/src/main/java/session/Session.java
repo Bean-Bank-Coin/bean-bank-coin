b69c81770e774bfcaebe434d5ffe25d85b00717f
Verushan Naidoo <verushan.naidoo@bbd.co.za>
2024-03-13 22:00:48 +0200

    Added password hiding and repeated prompting for duplicate entries

--- app/cli/src/main/java/session/Session.java
+++ app/cli/src/main/java/session/Session.java
@@ -26,52 +26,6 @@ public class Session {
     private static final String WELCOME_USER_PROMPT_START = "Welcome, ";
     private static final String WELCOME_USER_PROMPT_END = "!";
     private static final String USER_NOT_FOUND_MESSAGE = "Username not found.";
-    private static final String WELCOME_TO_THE_CLI = " __    __    ___  _         __   ___   ___ ___    ___      ______   ___                   \r\n"
-            + //
-            "|  T__T  T  /  _]| T       /  ] /   \\ |   T   T  /  _]    |      T /   \\                  \r\n" + //
-            "|  |  |  | /  [_ | |      /  / Y     Y| _   _ | /  [_     |      |Y     Y                 \r\n" + //
-            "|  |  |  |Y    _]| l___  /  /  |  O  ||  \\_/  |Y    _]    l_j  l_j|  O  |                 \r\n" + //
-            "l  `  '  !|   [_ |     T/   \\_ |     ||   |   ||   [_       |  |  |     |                 \r\n" + //
-            " \\      / |     T|     |\\     |l     !|   |   ||     T      |  |  l     !                 \r\n" + //
-            "  \\_/\\_/  l_____jl_____j \\____j \\___/ l___j___jl_____j      l__j   \\___/                  \r\n" + //
-            "                                                                                          \r\n" + //
-            " ____     ___   ____  ____       ____    ____  ____   __  _         __   ___  ____  ____  \r\n" + //
-            "|    \\   /  _] /    T|    \\     |    \\  /    T|    \\ |  l/ ]       /  ] /   \\l    j|    \\ \r\n" + //
-            "|  o  ) /  [_ Y  o  ||  _  Y    |  o  )Y  o  ||  _  Y|  ' /       /  / Y     Y|  T |  _  Y\r\n" + //
-            "|     TY    _]|     ||  |  |    |     T|     ||  |  ||    \\      /  /  |  O  ||  | |  |  |\r\n" + //
-            "|  O  ||   [_ |  _  ||  |  |    |  O  ||  _  ||  |  ||     Y    /   \\_ |     ||  | |  |  |\r\n" + //
-            "|     ||     T|  |  ||  |  |    |     ||  |  ||  |  ||  .  |    \\     |l     !j  l |  |  |\r\n" + //
-            "l_____jl_____jl__j__jl__j__j    l_____jl__j__jl__j__jl__j\\_j     \\____j \\___/|____jl__j__j\r\n" + //
-            "                                                                                          \r\n" + //
-            "                                                                                          \r\n" + //
-            "                                                                                          ";
-
-    private static final String GOODBYE_COMMAND = " ______  __ __   ____  ____   __  _      __ __   ___   __ __      _____   ___   ____      \r\n"
-            + //
-            "|      T|  T  T /    T|    \\ |  l/ ]    |  T  T /   \\ |  T  T    |     | /   \\ |    \\     \r\n" + //
-            "|      ||  l  |Y  o  ||  _  Y|  ' /     |  |  |Y     Y|  |  |    |   __jY     Y|  D  )    \r\n" + //
-            "l_j  l_j|  _  ||     ||  |  ||    \\     |  ~  ||  O  ||  |  |    |  l_  |  O  ||    /     \r\n" + //
-            "  |  |  |  |  ||  _  ||  |  ||     Y    l___, ||     ||  :  |    |   _] |     ||    \\     \r\n" + //
-            "  |  |  |  |  ||  |  ||  |  ||  .  |    |     !l     !l     |    |  T   l     !|  .  Y    \r\n" + //
-            "  l__j  l__j__jl__j__jl__j__jl__j\\_j    l____/  \\___/  \\__,_j    l__j    \\___/ l__j\\_j    \r\n" + //
-            "                                                                                          \r\n" + //
-            " ____    ____  ____   __  _  ____  ____    ____      __    __  ____  ______  __ __        \r\n" + //
-            "|    \\  /    T|    \\ |  l/ ]l    j|    \\  /    T    |  T__T  Tl    j|      T|  T  T       \r\n" + //
-            "|  o  )Y  o  ||  _  Y|  ' /  |  T |  _  YY   __j    |  |  |  | |  T |      ||  l  |       \r\n" + //
-            "|     T|     ||  |  ||    \\  |  | |  |  ||  T  |    |  |  |  | |  | l_j  l_j|  _  |       \r\n" + //
-            "|  O  ||  _  ||  |  ||     Y |  | |  |  ||  l_ |    l  `  '  ! |  |   |  |  |  |  |       \r\n" + //
-            "|     ||  |  ||  |  ||  .  | j  l |  |  ||     |     \\      /  j  l   |  |  |  |  |       \r\n" + //
-            "l_____jl__j__jl__j__jl__j\\_j|____jl__j__jl___,_j      \\_/\\_/  |____j  l__j  l__j__j       \r\n" + //
-            "                                                                                          \r\n" + //
-            " ____     ___   ____  ____       ____    ____  ____   __  _         __   ___  ____  ____  \r\n" + //
-            "|    \\   /  _] /    T|    \\     |    \\  /    T|    \\ |  l/ ]       /  ] /   \\l    j|    \\ \r\n" + //
-            "|  o  ) /  [_ Y  o  ||  _  Y    |  o  )Y  o  ||  _  Y|  ' /       /  / Y     Y|  T |  _  Y\r\n" + //
-            "|     TY    _]|     ||  |  |    |     T|     ||  |  ||    \\      /  /  |  O  ||  | |  |  |\r\n" + //
-            "|  O  ||   [_ |  _  ||  |  |    |  O  ||  _  ||  |  ||     Y    /   \\_ |     ||  | |  |  |\r\n" + //
-            "|     ||     T|  |  ||  |  |    |     ||  |  ||  |  ||  .  |    \\     |l     !j  l |  |  |\r\n" + //
-            "l_____jl_____jl__j__jl__j__j    l_____jl__j__jl__j__jl__j\\_j     \\____j \\___/|____jl__j__j\r\n" + //
-            "                                                                                          ";
-
     private static final String LOGOUT_COMMAND = "-logout";
     private static final String ERROR_COMMAND = "error";
     private static final String HELP_COMMAND = "-help";
@@ -96,9 +50,9 @@ public class Session {
         String userOption = inputHandler.handleUserInput(scanner, LOG_IN_SIGN_UP_PROMPT,
                 Arrays.asList("L", "R"));
 
-        if (userOption.equals("L") || userOption.equals("l")) {
+        if (userOption.equals("L")) {
             String username = inputHandler.handleUserInput(scanner, USERNAME_PROMPT, Collections.emptyList());
-            String password = new String(console.readPassword("Enter your password: "));
+            String password = new String(console.readPassword("Enter a password: "));
 
             Optional<User> userOptional = UserRequest.getInstance().getUser(username);
 
@@ -117,19 +71,20 @@ public class Session {
 
         String username = inputHandler.handleUserInput(scanner, USERNAME_PROMPT, Collections.emptyList());
         String email = inputHandler.handleUserInput(scanner, "Enter your email: ", Collections.emptyList());
-        String password = inputHandler.handleUserInput(scanner, "Enter your password: ", Collections.emptyList());
+        String password = new String(console.readPassword("Enter a password: "));
 
         Optional<User> userOptional = UserRequest.getInstance().getUser(username);
 
-        if (userOptional.isPresent())
+        while (userOptional.isPresent()) {
             System.out.println("Username already exists.");
+            username = inputHandler.handleUserInput(scanner, USERNAME_PROMPT, Collections.emptyList());
+            userOptional = UserRequest.getInstance().getUser(username);
+        }
 
         return UserRequest.getInstance().createUser(username, password, email);
     }
 
     public String startSession(Scanner scanner) {
-
-        System.out.println(WELCOME_TO_THE_CLI);
 
         User currUser = signInOrRegisterUser(scanner);
 
