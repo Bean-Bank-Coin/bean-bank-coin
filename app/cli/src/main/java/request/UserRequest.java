fe569ee2e479211dff36f7d2d803e6c32a5e78fc
Verushan Naidoo <verushan.naidoo@bbd.co.za>
2024-03-13 15:59:56 +0200

    Added login and registration

--- app/cli/src/main/java/request/UserRequest.java
+++ app/cli/src/main/java/request/UserRequest.java
@@ -22,8 +22,30 @@ public class UserRequest {
                 userObject.getInt("userID"),
                 userObject.getString("username"),
                 userObject.getString("password"),
-                userObject.getString("email")
-        ));
+                userObject.getString("email")));
+    }
+
+    public User createUser(String username, String password, String email) {
+        JSONObject payload = new JSONObject();
+        payload.put("username", username);
+        payload.put("password", password);
+        payload.put("email", email);
+
+        System.out.println(payload.toString());
+        Request request = new Request();
+        HttpResponse<String> response = request.makeRequest("user", RequestType.POST, Optional.of(payload));
+
+        if (response.body().equals(""))
+            return null;
+
+        System.out.println(response.body());
+        JSONObject userObject = new JSONObject(response.body());
+
+        return new User(
+                userObject.getInt("userID"),
+                userObject.getString("username"),
+                userObject.getString("password"),
+                userObject.getString("email"));
     }
 
     private UserRequest() {
--- app/cli/src/main/java/session/Session.java
+++ app/cli/src/main/java/session/Session.java
@@ -5,7 +5,11 @@ import com.models.BeanType;
 import com.models.User;
 
 import request.UserRequest;
+import util.UserInputHandler;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
@@ -18,7 +22,6 @@ public class Session {
     private List<Account> userAccounts;
 
     private static final String LINE_PROMPT = "> ";
-    private static final String USERNAME_PROMPT = "Enter your username: ";
     private static final String WELCOME_USER_PROMPT_START = "Welcome, ";
     private static final String WELCOME_USER_PROMPT_END = "!";
     private static final String USER_NOT_FOUND_MESSAGE = "Username not found.";
@@ -29,18 +32,53 @@ public class Session {
 
     }
 
-    public String startSession(Scanner scanner) {
-        System.out.print(USERNAME_PROMPT);
-        String username = scanner.nextLine();
+    public User signInOrRegisterUser(Scanner scanner) {
+        final String LOG_IN_SIGN_UP_PROMPT = "Login (L) or Register (R) ";
+        final String USERNAME_PROMPT = "Enter your username: ";
+
+        UserInputHandler inputHandler = UserInputHandler.getInstance();
+        String userOption = inputHandler.handleUserInput(scanner, LOG_IN_SIGN_UP_PROMPT,
+                Arrays.asList("L", "R"));
+
+        if (userOption.equals("L")) {
+            String username = inputHandler.handleUserInput(scanner, USERNAME_PROMPT, Collections.emptyList());
+            String password = inputHandler.handleUserInput(scanner, "Enter your password: ", Collections.emptyList());
+
+            Optional<User> userOptional = UserRequest.getInstance().getUser(username);
+
+            if (!userOptional.isPresent()) {
+                System.out.println(USER_NOT_FOUND_MESSAGE);
+                return null;
+            }
+
+            if (!password.equals(userOptional.get().getPassword())) {
+                System.out.println("Incorrect password");
+                return null;
+            }
+
+            return userOptional.get();
+        }
+
+        String username = inputHandler.handleUserInput(scanner, USERNAME_PROMPT, Collections.emptyList());
+        String email = inputHandler.handleUserInput(scanner, "Enter your email: ", Collections.emptyList());
+        String password = inputHandler.handleUserInput(scanner, "Enter your password: ", Collections.emptyList());
 
         Optional<User> userOptional = UserRequest.getInstance().getUser(username);
 
-        if (!userOptional.isPresent()) {
-            System.out.println(USER_NOT_FOUND_MESSAGE);
-            return ERROR_COMMAND;
+        if (userOptional.isPresent())
+            System.out.println("Username already exists.");
+
+        return UserRequest.getInstance().createUser(username, password, email);
+    }
+
+    public String startSession(Scanner scanner) {
+        User currUser = signInOrRegisterUser(scanner);
+
+        if (currUser == null) {
+            return LOGOUT_COMMAND;
         }
 
-        System.out.println(WELCOME_USER_PROMPT_START + userOptional.get().getUsername() + WELCOME_USER_PROMPT_END);
+        System.out.println(WELCOME_USER_PROMPT_START + currUser.getUsername() + WELCOME_USER_PROMPT_END);
 
         System.out.print(LINE_PROMPT);
         String userInput = scanner.nextLine();
